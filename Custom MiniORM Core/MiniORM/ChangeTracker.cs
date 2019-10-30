1b83e05c36b0a6e2c7ca7de9e3dd3d9bb7dcdc87
rptlonfire <aduhowni123@gmail.com>
2019-10-30 17:42:29 +0000

    Custom Mini ORM

---
+++ Custom MiniORM Core/MiniORM/ChangeTracker.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace MiniORM
+{
+    internal class ChangeTracker<T>
+        where T : class, new()
+    {
+        private readonly List<T> allEntities;
+
+        private readonly List<T> added;
+
+        private readonly List<T> removed;
+
+        public ChangeTracker(IEnumerable<T> entities)
+        {
+            this.added = new List<T>();
+
+            this.removed = new List<T>();
+
+            this.allEntities = CloneEntities(entities);
+        }
+
+        public IReadOnlyCollection<T> AllEntities => this.allEntities.AsReadOnly();
+
+        public IReadOnlyCollection<T> Added => this.added.AsReadOnly();
+
+        public IReadOnlyCollection<T> Removed => this.removed.AsReadOnly();
+
+        public void Add(T item) => this.added.Add(item);
+
+        public void Remove(T item) => this.removed.Add(item);
+
+
+        private static List<T> CloneEntities(IEnumerable<T> entities)
+        {
+            var clonedEntities = new List<T>();
+
+            PropertyInfo[] propertiesToClone = typeof(T).GetProperties().
+                Where(p => DbContext.AllowedSqlTypes.Contains(p.PropertyType)).ToArray();
+
+            foreach (var entity in entities)
+            {
+                T clonedEntity = Activator.CreateInstance<T>();
+
+                foreach (var property in propertiesToClone)
+                {
+                    var value = property.GetValue(entity);
+                    property.SetValue(clonedEntity, value);
+                }
+
+                clonedEntities.Add(clonedEntity);
+
+            }
+
+            return clonedEntities;
+        }
+    }
+
+}
