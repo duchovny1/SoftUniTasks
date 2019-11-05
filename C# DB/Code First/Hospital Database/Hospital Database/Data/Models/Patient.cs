71d76dd1f1cdf1e65766ab4be57b81b74cc253f7
rptlonfire <aduhowni123@gmail.com>
2019-11-05 19:12:20 +0000

    Code First/ Creating and configurating database

---
+++ C# DB/Code First/Hospital Database/Hospital Database/Data/Models/Patient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+
+
+        [Required]
+        [MaxLength(50)]
+
+        public string FirstName { get; set; }
+
+
+        [Required]
+        [MaxLength(50)]
+
+        public string LastName { get; set; }
+
+        [MaxLength(250)]
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        [Required]
+        public bool HasInsurance { get; set; }
+
+        public ICollection<PatientMedicament> Prescriptions { get; set; }
+
+        public ICollection<Diagnose> Diagnoses { get; set; }
+        public ICollection<Visitation> Visitations { get; set; } = new List<Visitation>();
+
+    }
+}
---
+++ C# DB/Code First/Hospital Database/Hospital Database/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_HospitalDatabase.Data;
+using System;
+
+namespace P01_HospitalDatabase
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            HospitalContext context = new HospitalContext();
+
+            context.Database.Migrate();
+        }
+    }
+}
