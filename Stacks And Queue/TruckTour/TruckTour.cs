aa934bcde57b842bd58a97ee5e692f9f4f6ae7c2
rptlonfire <aduhowni123@gmail.com>
2019-11-09 18:24:51 +0000

    Stacks And Queues Exercises

---
+++ Stacks And Queue/TruckTour/TruckTour.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Truck_Tour
+{
+    class TruckTour
+    {
+        static void Main(string[] args)
+        {
+            var input = int.Parse(Console.ReadLine());
+
+            Queue<int[]> petrolPumps = new Queue<int[]>();
+
+
+            int index = 0;
+            for (int i = 0; i < input; i++)
+            {
+                int[] line = Console.ReadLine()
+                     .Split()
+                     .Select(int.Parse)
+                     .ToArray();
+
+                petrolPumps.Enqueue(line);
+            }
+
+            while (true)
+            {
+
+                int petrol = 0;
+                foreach (var petrolPump in petrolPumps)
+                {
+
+                    int amountOfPetrol = petrolPump[0];
+                    int distanceToNextPump = petrolPump[1];
+
+                    petrol += (amountOfPetrol - distanceToNextPump);
+
+                    if (petrol < 0)
+                    {
+                        petrolPumps.Enqueue(petrolPumps.Dequeue());
+                        index++;
+                        break;
+                    }
+
+                }
+                if (petrol >= 0)
+                {
+                    break;
+                }
+
+            }
+
+            Console.WriteLine(index);
+        }
+    }
+}
