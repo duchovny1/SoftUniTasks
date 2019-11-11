c0940ade39b10045702a1dfedb33b83d669abc5c
rptlonfire <aduhowni123@gmail.com>
2019-11-11 21:04:14 +0000

    Entity Relations

---
+++ C# DB/Entity Relations/Student System/Data/Models/Resource.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Resource
+    {
+        public int ResourceId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+        [Required]
+        public ResourceType ResourceType { get; set; }
+
+        [Required]
+        public int CourseId { get; set; }
+
+        public Course Course { get; set; }
+
+
+    }
+}
