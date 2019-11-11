73ffa437f44953517cb6f121e3ae01e006999a1c
rptlonfire <aduhowni123@gmail.com>
2019-11-11 21:03:10 +0000

    Create ResourceContext.cs

---
+++ C# DB/Entity Relations/Student System/Data/Configurations/ResourceContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using P01_StudentSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student_System.Data.Configurations
+{
+    public class ResourceContext : IEntityTypeConfiguration<Resource>
+    {
+        public void Configure(EntityTypeBuilder<Resource> builder)
+        {
+            builder.Property(x => x.Url)
+                .IsUnicode
+                (false);
+
+            builder.HasOne(r => r.Course)
+                .WithMany(c => c.Resources)
+                .HasForeignKey(fk => fk.ResourceId);
+
+
+        }
+    }
+}
