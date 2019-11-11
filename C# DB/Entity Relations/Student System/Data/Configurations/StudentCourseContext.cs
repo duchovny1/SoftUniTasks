c4d4d198431f7c2fdad0ac19179989fa583d5b5d
rptlonfire <aduhowni123@gmail.com>
2019-11-11 21:03:33 +0000

    Create StudentCourseContext.cs

---
+++ C# DB/Entity Relations/Student System/Data/Configurations/StudentCourseContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using P01_StudentSystem.Data.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student_System.Data.Configurations
+{
+   public class StudentCourseContext : IEntityTypeConfiguration<StudentCourse>
+    {
+        public void Configure(EntityTypeBuilder<StudentCourse> builder)
+        {
+            builder.HasKey(e => new { e.StudentId, e.CourseId });
+
+            builder.HasOne(e => e.Student)
+                .WithMany(s => s.CourseEnrollments)
+                .HasForeignKey(fk => fk.StudentId);
+
+            builder.HasOne(e => e.Course)
+                .WithMany(c => c.StudentsEnrolled)
+                .HasForeignKey(fk => fk.CourseId);
+
+
+        }
+    }
+}
