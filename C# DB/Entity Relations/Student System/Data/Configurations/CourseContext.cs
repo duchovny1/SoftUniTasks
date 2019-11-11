c1abc8a7b134d04c70df07fc7d98ae35434acbf8
rptlonfire <aduhowni123@gmail.com>
2019-11-11 21:02:51 +0000

    Create CourseContext.cs

---
+++ C# DB/Entity Relations/Student System/Data/Configurations/CourseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using P01_StudentSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student_System.Data.Configurations
+{
+    public class CourseContext : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> builder)
+        {
+            
+        }
+    }
+}
