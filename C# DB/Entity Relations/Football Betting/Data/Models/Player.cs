d8948db0d81dc9071a1b8aa1d70cbf2700ed7d6a
rptlonfire <aduhowni123@gmail.com>
2019-11-12 17:14:09 +0000

    Create Player.cs

---
+++ C# DB/Entity Relations/Football Betting/Data/Models/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+
+        [Required]
+        public bool IsInjured { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Name { get; set; }
+
+        [Required]
+        public int PositionId { get; set; }
+
+        public Position Position { get; set; }
+
+        [Required]
+        public byte SquadNumber { get; set; }// it might need to be int
+
+
+        public int TeamId { get; set; }
+
+        public Team Team { get; set; }
+
+        public ICollection<PlayerStatistic> PlayerStatistics { get; set; } = new HashSet<PlayerStatistic>();
+    }
+}
