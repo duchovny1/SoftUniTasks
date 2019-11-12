e9e8158a4f3321a92d7f2d7badb8b6fa859ef337
rptlonfire <aduhowni123@gmail.com>
2019-11-12 17:13:21 +0000

    Create Country.cs

---
+++ C# DB/Entity Relations/Football Betting/Data/Models/Country.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace P03_FootballBetting.Data.Models
+{
+
+    public class Country
+    {
+        public int CountryId { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+
+        public string Name { get; set; }
+
+        public ICollection<Town> Towns { get; set; } = new HashSet<Town>(); //many towns in one contry
+    }
+}
