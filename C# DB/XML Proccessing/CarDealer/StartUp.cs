95b9f3ba574eecf51cf0b21aecca853fa62cb1a4
rptlonfire <aduhowni123@gmail.com>
2019-11-26 16:37:16 +0000

    XML Proccessing And Querying

---
+++ C# DB/XML Proccessing/CarDealer/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using CarDealer.Data;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new CarDealerContext())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+            }
+        }
+    }
+}
