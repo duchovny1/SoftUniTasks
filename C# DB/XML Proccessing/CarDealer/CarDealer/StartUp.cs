75807a6af1be62fe09ea90d3ec8e3b67e0ecc228
rptlonfire <aduhowni123@gmail.com>
2019-11-27 17:25:23 +0000

    ьяаьяаь

--- C# DB/XML Proccessing/CarDealer/CarDealer/StartUp.cs
+++ C# DB/XML Proccessing/CarDealer/CarDealer/StartUp.cs
@@ -1,4 +1,9 @@
 ﻿using CarDealer.Data;
+using CarDealer.Dtos.Import;
+using CarDealer.Models;
+using System.IO;
+using System.Xml.Serialization;
+using AutoMapper;
 
 namespace CarDealer
 {
@@ -6,11 +11,38 @@ namespace CarDealer
     {
         public static void Main(string[] args)
         {
+            Mapper.Initialize(x =>
+            {
+                x.AddProfile<CarDealerProfile>();
+            });
+
             using (var db = new CarDealerContext())
             {
-                db.Database.EnsureDeleted();
-                db.Database.EnsureCreated();
+                var inputXml = File.ReadAllText("./../../../Datasets/suppliers.xml");
             }
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            var rootElement = new XmlRootAttribute("Suppliers");
+
+            var serializer = new XmlSerializer(typeof(DTO_ImportSuppliers[]), rootElement);
+
+            using (var reader = new StringReader(inputXml))
+            {
+                var suppliers = (DTO_ImportSuppliers[])serializer.Deserialize(reader);
+
+                foreach (var dto in suppliers)
+                {
+                    var supplier = Mapper.Map<Supplier>(dto);
+
+                    context.Suppliers.Add(supplier);
+                }
+
+
+            }
+            int count = context.SaveChanges();
+            return $"Successfully imported {count}";
         }
     }
 }
