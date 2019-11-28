59be4e9cf1584566eea00b763b34430d8df9d139
rptlonfire <aduhowni123@gmail.com>
2019-11-28 19:20:16 +0000

    Design Patterns/Command Pattern

---
+++ Design Patterns/Design Patterns/Command Pattern/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Command_Pattern
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public Product(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public void IncreasePrice(int amount)
+        {
+            Price += amount;
+            Console.WriteLine($"The price for {Name} has been increased by {amount}$.");
+        }
+
+        public void DecreasePrice(int amount)
+        {
+            if (Price > amount)
+            {
+                Price -= amount;
+                Console.WriteLine($"The price for {Name} has been decrased by {amount}$.");
+            }
+        }
+
+        public override string ToString() => $"Current price for {Name} product is {Price}$.";
+       
+
+    }
+}
