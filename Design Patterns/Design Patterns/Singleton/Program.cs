a7cb1e64814a3f445f260ee42a008f6299cbc483
rptlonfire <aduhowni123@gmail.com>
2019-11-28 16:09:41 +0000

    Design Patterns

---
+++ Design Patterns/Design Patterns/Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db1 = SingleDataContainer.Instance;
+            var db2 = SingleDataContainer.Instance;
+            var db3 = SingleDataContainer.Instance;
+            var db4 = SingleDataContainer.Instance;
+
+        }
+    }
+}
---
+++ Design Patterns/Design Patterns/Singleton/SingleDataContainer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Singleton
+{
+    public class SingleDataContainer : ISingletonContainer
+    {
+        private Dictionary<string, int> capitals = new Dictionary<string, int>();
+
+        private static SingleDataContainer instance = new SingleDataContainer();
+
+        private SingleDataContainer()
+        {
+            Console.WriteLine($"Initilizing singleton object");
+
+            //var elements = File.ReadAllLines("capitals.txt");
+
+            //for (int i = 0; i < elements.Length; i++)
+            //{
+            //    capitals.Add(elements[i], int.Parse(elements[i] + 1));
+            //}
+        }
+
+        public static SingleDataContainer Instance => instance;
+
+
+
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
+        }
+    }
+}
