982e78a03ba0155089e86c72856962597fbda7c7
rptlonfire <aduhowni123@gmail.com>
2019-11-30 20:17:32 +0000

    Create Program.cs

---
+++ Text Processing and Regular Expressions/Regex/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ");
+
+              List<string> result = new List<string>();
+
+            bool length = false;
+            bool contains = false;
+            bool symbols = false;
+            
+
+            for (int i = 0; i < usernames.Length; i++)
+            {
+                if (usernames[i].Length <= 16 && usernames[i].Length >= 3)
+                {
+                    length = true;
+                }
+                if (Regex.IsMatch())
+                { }
+                Reg
+            }
+
+
+        }
+    }
+}
