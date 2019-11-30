36d06eb6d1f22a37606d3aa0a37a220ff87b6522
rptlonfire <aduhowni123@gmail.com>
2019-11-30 20:15:12 +0000

    Create Program.cs

---
+++ Text Processing and Regular Expressions/Matching Names/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string regex = @"[+]359([ -])2\1\d{3}\1\d{4}\b";
+            string text = Console.ReadLine();
+
+            MatchCollection match = Regex.Matches(text, regex);
+            List<string> phones = new List<string>();
+
+            foreach (Match name in match)
+            {
+
+                phones.Add(name.Value);
+            }
+
+            Console.Write(string.Join(", ", phones));
+        }
+    }
+}
