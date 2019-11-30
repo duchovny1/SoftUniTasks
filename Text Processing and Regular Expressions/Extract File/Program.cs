6ff501da100965e755fc9d4ec461de322a535301
rptlonfire <aduhowni123@gmail.com>
2019-11-30 20:14:56 +0000

    Create Program.cs

---
+++ Text Processing and Regular Expressions/Extract File/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+
+            int startingIndex = path.LastIndexOf('\\') + 1;
+
+            string file = path.Substring(startingIndex);
+
+            string[] allFileName = file.Split('.');
+
+            Console.WriteLine($"File name: {allFileName[0]}");
+            Console.WriteLine($"File extension: {allFileName[1]}");
+
+
+
+
+        }
+    }
+}
