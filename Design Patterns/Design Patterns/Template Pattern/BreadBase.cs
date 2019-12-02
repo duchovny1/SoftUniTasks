1cbf1e37d5f99f5e4d683d65ee5fdafc9228f531
rptlonfire <aduhowni123@gmail.com>
2019-12-02 15:53:23 +0000

    Template Pattern

---
+++ Design Patterns/Design Patterns/Template Pattern/BreadBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template_Pattern
+{
+    class BreadBase
+    {
+    }
+}
---
+++ Design Patterns/Design Patterns/Template Pattern/WholeWhear.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template_Pattern
+{
+    public class WholeWheat : BreadBase
+    {
+        public override void Bake()
+        {
+            Console.WriteLine($"Baking whole wheat bread for 20 minutes");
+        }
+
+        public override void MixIngridients()
+        {
+            Console.WriteLine($"gathering ingredients for whole wheat bread");
+        }
+    }
+}
