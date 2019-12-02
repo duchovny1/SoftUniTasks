9c1a754d60719ba2637558dd63f2774c82788f4f
rptlonfire <aduhowni123@gmail.com>
2019-12-02 18:56:41 +0000

    Xml proccessing

--- C# DB/XML Proccessing/CarDealer/CarDealer/CarDealerProfile.cs
+++ C# DB/XML Proccessing/CarDealer/CarDealer/CarDealerProfile.cs
@@ -9,6 +9,7 @@ namespace CarDealer
         public CarDealerProfile()
         {
             this.CreateMap<DTO_ImportSuppliers, Supplier>();
+            this.CreateMap<DTO_ImportCars, Car>();
         }
     }
 }
