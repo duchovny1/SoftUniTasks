6111bad1d44840fadae95942d5d5e1bd3b35e113
rptlonfire <aduhowni123@gmail.com>
2019-12-03 21:36:09 +0000

    Update CarDealerProfile.cs

--- C# DB/XML Proccessing/CarDealer/CarDealer/CarDealerProfile.cs
+++ C# DB/XML Proccessing/CarDealer/CarDealer/CarDealerProfile.cs
@@ -2,6 +2,7 @@
 using CarDealer.Dtos.Export;
 using CarDealer.Dtos.Import;
 using CarDealer.Models;
+using System.Linq;
 
 namespace CarDealer
 {
@@ -12,6 +13,10 @@ namespace CarDealer
             this.CreateMap<ImportSupplierDto, Supplier>();
             this.CreateMap<ImportPartDto, Part>();
 
+
+            this.CreateMap<Part, ExportPartsDto>();
+            this.CreateMap<Car, ExportCarsWithParts>()
+                .ForMember(x => x.Parts, y => y.MapFrom(x => x.PartCars.Select(p => p.Part)));
             this.CreateMap<Supplier, ExportLocalSuppliersDto>();
         }
     }
