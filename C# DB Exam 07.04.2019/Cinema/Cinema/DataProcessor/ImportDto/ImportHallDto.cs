3f38a8d5c8d5651401f0068b763437055ace6512
rptlonfire <aduhowni123@gmail.com>
2019-12-08 19:17:59 +0000

    Create ImportHallDto.cs

---
+++ C# DB Exam 07.04.2019/Cinema/Cinema/DataProcessor/ImportDto/ImportHallDto.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Cinema.DataProcessor.ImportDto
+{
+    public class ImportHallDto
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string Name { get; set; }
+        public bool Is4Dx { get; set; }
+        public bool Is3D { get; set; }
+
+
+        [JsonProperty("Seats")]
+        [Range(1, int.MaxValue)]
+        public int SeatsCount { get; set; }
+    }
+}
