3eb0d562723517c6dbdb3af5ba4c8ddb56b2921e
rptlonfire <aduhowni123@gmail.com>
2019-12-08 19:18:52 +0000

    Create ImportProjectionDto.cs

---
+++ C# DB Exam 07.04.2019/Cinema/Cinema/DataProcessor/ImportDto/ImportProjectionDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Cinema.DataProcessor.ImportDto
+{
+   public class ImportProjectionDto
+    {
+        [Required]
+        public int MovieId { get; set; }
+
+        [Required]
+        public int HallId { get; set; } 
+
+        [Required]
+        public DateTime DateTime { get; set; }
+    }
+}
