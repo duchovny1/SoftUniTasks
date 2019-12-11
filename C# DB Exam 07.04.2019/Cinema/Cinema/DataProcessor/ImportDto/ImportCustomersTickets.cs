c1f2645de4f277cbc4c4ddaacbc1802ed61ff1bf
rptlonfire <aduhowni123@gmail.com>
2019-12-11 21:01:41 +0000

    dtos updated

---
+++ C# DB Exam 07.04.2019/Cinema/Cinema/DataProcessor/ImportDto/ImportCustomersTickets.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Cinema.DataProcessor.ImportDto
+{
+    [XmlType("Customer")]
+    public class ImportCustomersTickets
+    {
+        [XmlElement("FirstName")]
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string FirstName { get; set; }
+
+        [XmlElement("LastName")]
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        [XmlElement("Age")]
+        [Required]
+        [Range(12, 110)]
+        public int Age { get; set; }
+
+        [XmlElement("Balance")]
+        [Required]
+        [Range(typeof(decimal), "0.01", "100000000000")]
+        public decimal Balance { get; set; }
+
+        [XmlArray("Tickets")]
+        public ImportTicketDto[] Tickets { get; set; }
+    }
+}
---
+++ C# DB Exam 07.04.2019/Cinema/Cinema/DataProcessor/ImportDto/ImportTicketDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Cinema.DataProcessor.ImportDto
+{
+
+    [XmlType("Ticket")]
+    public class ImportTicketDto
+    {
+        [XmlElement("ProjectionId")]
+        [Required]
+        public int ProjectionId { get; set; }
+
+
+        [XmlElement("Price")]
+        [Required]
+        [Range(typeof(decimal), "0.01", "100000000000")]
+        public decimal Price { get; set; }
+    }
+}
