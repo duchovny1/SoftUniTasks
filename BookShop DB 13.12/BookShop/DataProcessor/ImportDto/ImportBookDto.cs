db936733a9b9faf39982839c0210fb8979ae9c70
rptlonfire <aduhowni123@gmail.com>
2019-12-13 19:12:35 +0000

    DB Exam 13.12.2019 Models uploaded

---
+++ BookShop DB 13.12/BookShop/DataProcessor/ImportDto/ImportBookDto.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace BookShop.DataProcessor.ImportDto
+{
+
+    [XmlType("Book")]
+    public class ImportBookDto
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [XmlElement("Genre")]
+        public string Genre { get; set; }
+
+        [Range(typeof(decimal), "0.01", "79228162514264337593543950335")]
+        [XmlElement("Price")]
+        public decimal Price { get; set; }
+
+
+        [Range(50, 5000)]
+        [XmlElement("Pages")]
+        public int Pages { get; set; }
+
+        [Required]
+        [XmlElement("PublishedOn")]
+        public string PublishedOn { get; set; }
+    }
+}
---
+++ BookShop DB 13.12/BookShop/DataProcessor/ImportDto/ImportBookJsonDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.DataProcessor.ImportDto
+{
+    public class ImportBookJsonDto
+    {
+        public string Id { get; set; }
+    }
+}
